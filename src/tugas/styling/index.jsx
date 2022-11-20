7c30ec8c4d179cebcfc1bc85d7cf2f92afc8e4be
LAPTOP-GCFV47F1\ASUS <yusufjojo244@gmail.com>
2022-11-21 02:51:45 +0700

    tambah bootstrap

---
+++ src/tugas/styling/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Portofolio from "./portofolio"
+
+export default class Styling extends React.Component {
+    render() {
+        return {
+           
+             <Portofolio />
+            
+        }
+    }
+}
--- src/tugas/styling/portofolio/index.jsx
+++ src/tugas/styling/portofolio/index.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Nav } from "react-bootstrap";
 
 export default class Portofolio extends React.Component {
   render() {
-    return {};
+    return {
+         <Nav defaultActiveKey="/home" as="ul">
+            <Nav.Item as="li">
+                <Nav.Link href="/home">Active</Nav.Link>
+            </Nav.Item>
+            <Nav.Item as="li">
+                <Nav.Link eventKey="link-1">Link</Nav.Link>
+            </Nav.Item>
+            <Nav.Item as="li">
+                <Nav.Link eventKey="link-2">Link</Nav.Link>
+            </Nav.Item>
+    </Nav>
+    };
   }
 }
